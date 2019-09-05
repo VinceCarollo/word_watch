ffe0b7a942ac9a90a339d9f22362431bf934c1c3
Vince Carollo <vincecarollo@gmail.com>
2019-09-05 11:23:10 -0600

    Add ability for user to add individual or multiple words on home page

--- src/index.js
+++ src/index.js
@@ -1,18 +1,42 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  $.get('http://localhost:3000/api/v1/top_word', (data) => {
-    appendTopWord(data.word);
-  })
+  var loadTopWord = () => {
+    $.get('http://localhost:3000/api/v1/top_word', (data) => {
+      showTopWord(data.word);
+    })
+  }
 
-  var appendTopWord = (word) => {
-    let top_word = Object.keys(word)[0].charAt(0).toUpperCase() + Object.keys(word)[0].slice(1)
+  var showTopWord = (word) => {
+    $('.word-count').children().last().remove();
+    $('.word-count').children().last().remove();
+    let top_word = Object.keys(word)[0].charAt(0).toUpperCase() + Object.keys(word)[0].slice(1);
     let word_count = word[Object.keys(word)[0]];
     $('.word-count').append(`
       <h2>${top_word}</h2>
-      `)
-    $('.word-count').append(`
       <p>Count: ${word_count}</p>
       `)
   }
+
+  var clearSuccessMessage = () => {
+    setTimeout(() => {
+      $('#success-message').children().remove()
+    }, 3000)
+  }
+
+  $('#word-submit').click(() => {
+    let text = $.trim($("#word-text").val());
+    text.split(' ').forEach(word => {
+      $.post('http://localhost:3000/api/v1/words', { word: { value: word } }, (data) => {
+        $('#success-message').append(`
+          <p>${data['message']}</p>
+          `)
+      })
+    })
+    $("#word-text").val('')
+    loadTopWord();
+    clearSuccessMessage();
+  })
+
+  loadTopWord();
 })
