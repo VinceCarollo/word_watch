79acf7d2b6f471d7f31b56b5ad368b6ed5393604
Vince Carollo <vincecarollo@gmail.com>
2019-09-05 10:44:40 -0600

    Add rendering of most used word and count of word on home page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $.get('http://localhost:3000/api/v1/top_word', (data) => {
+    appendTopWord(data.word);
+  })
+
+  var appendTopWord = (word) => {
+    let top_word = Object.keys(word)[0].charAt(0).toUpperCase() + Object.keys(word)[0].slice(1)
+    let word_count = word[Object.keys(word)[0]];
+    $('.word-count').append(`
+      <h2>${top_word}</h2>
+      `)
+    $('.word-count').append(`
+      <p>Count: ${word_count}</p>
+      `)
+  }
 })
